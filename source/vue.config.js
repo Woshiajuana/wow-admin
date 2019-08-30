b3be6b25543da1625b8f8cb75bb4a06978ac3d88
Woshiajuana <zhigang.chen@owulia.com>
2019-08-30 15:40:11 +0800

    subcode

--- source/vue.config.js
+++ source/vue.config.js
@@ -5,8 +5,6 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const port = process.env.port || process.env.npm_config_port || 9528 // dev port
-
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   publicPath: './',
