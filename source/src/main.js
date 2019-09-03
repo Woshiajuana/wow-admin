5beba01b5ce29dc02961b2d2036cd66907089cf5
Woshiajuana <zhigang.chen@owulia.com>
2019-09-03 17:45:54 +0800

    subcoded

--- source/src/main.js
+++ source/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+
+import '@/assets/scss/index.scss' // global css
+import '@/icons' // icon
+import '@/permission' // permission control
+
+import App from './App.vue'
+import store from './store'
+import router from './router'
+
+Vue.use(ElementUI, { locale });
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
