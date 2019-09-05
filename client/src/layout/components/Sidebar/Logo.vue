c254d5a2719d82974e6d4606afe7f383f3b8cb93
Woshiajuana <zhigang.chen@owulia.com>
2019-09-05 18:00:26 +0800

    subcode

--- client/src/layout/components/Sidebar/Logo.vue
+++ client/src/layout/components/Sidebar/Logo.vue
@@ -24,7 +24,7 @@
         },
         data() {
             return {
-                title: 'Vue Admin Template',
+                title: 'Wow-Admin Manage',
                 logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
             }
         }
