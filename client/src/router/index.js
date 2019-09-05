eb7dc631db3f6f28efed428ae20ba6c88e0ec4ab
Woshiajuana <zhigang.chen@owulia.com>
2019-09-05 14:07:33 +0800

    subcode

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@layout'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+NProgress.configure({ showSpinner: false });
 
 Vue.use(Router);
 
@@ -20,3 +23,31 @@ export const constantRoutes = [
     }
 
 ];
+
+const createRouter = () => new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+});
+
+const router = createRouter();
+
+export function resetRouter() {
+    const newRouter = createRouter();
+    router.matcher = newRouter.matcher; // reset router
+}
+
+router.beforeEach(async(to, from, next) => {
+    // start progress bar
+    NProgress.start();
+    next();
+    NProgress.done();
+});
+
+router.afterEach(() => {
+    // finish progress bar
+    NProgress.done()
+});
+
+
+export default router;
