99ac1dd8b05c4267107a4c5198c6a8d2666e948a
Woshiajuana <zhigang.chen@owulia.com>
2019-09-05 14:48:43 +0800

    subcode

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
+import '@assets/scss/index.scss'
 
 import App from '@/App.vue'
 import router from '@router'
