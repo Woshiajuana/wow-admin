6e28129abe9890413a70a519057977a00e91c0e1
Woshiajuana <zhigang.chen@owulia.com>
2019-09-05 15:52:01 +0800

    subcode

--- client/vue.config.js
+++ client/vue.config.js
@@ -27,4 +27,21 @@ module.exports = {
             }
         }
     },
+    chainWebpack (config) {
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/assets/icons'))
+            .end();
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/assets/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end();
+    },
 };
