3171d89d396c940713bb397a05703c93c73f6fc6
Woshiajuana <zhigang.chen@owulia.com>
2019-09-05 14:57:02 +0800

    subcode

--- client/src/main.js
+++ client/src/main.js
@@ -8,14 +8,17 @@ import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
 import '@assets/scss/index.scss'
+import '@/assets/icons' // icon
 
 import App from '@/App.vue'
+import store from '@store'
 import router from '@router'
 
 Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 new Vue({
+    store,
     router,
     render: h => h(App),
 }).$mount('#app');
