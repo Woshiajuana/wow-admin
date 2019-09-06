bdad8aa7bd4659a86f4ec1250ff394a568615e42
Woshiajuana <zhigang.chen@owulia.com>
2019-09-06 14:15:32 +0800

    subcode

---
+++ server/app/model/appInfoModel.js
@@ -0,0 +1,54 @@
+
+'use strict';
+
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+    const modelSchema = new Schema({
+
+        // 名称
+        name: {
+            type: String,
+            trim: true,
+            minlength: 2,
+            maxlength: 20,
+            default: '管理台',
+        },
+
+        // logo
+        logo: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+
+        // 主题
+        theme: {
+            type: String,
+            trim: true,
+            default: 'DEFAULT',
+        },
+
+        // 所有权
+        ownership: {
+            type: String,
+            trim: true,
+            maxlength: 100,
+            required: true,
+        },
+
+        // 创建时间
+        created_at: {
+            type: Date,
+            default: Date.now,
+        },
+
+        // 更新时间
+        updated_at: {
+            type: Date,
+            default: Date.now,
+        },
+
+    });
+    return mongoose.model('app_info', modelSchema);
+};
