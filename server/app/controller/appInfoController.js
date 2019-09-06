55fa4e83887308c72b5f3ee19fd72e286a4d2970
Woshiajuana <zhigang.chen@owulia.com>
2019-09-06 14:21:54 +0800

    subcode

--- server/app/controller/appInfoController.js
+++ server/app/controller/appInfoController.js
@@ -1,3 +1,14 @@
 
 'use strict';
 
+const { Controller } = require('egg');
+
+module.exports = class AppInfoController extends Controller {
+
+    // 查询信息
+    async info () {
+        const { ctx, service, app } = this;
+
+    }
+
+};
