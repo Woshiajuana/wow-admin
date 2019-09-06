7e80bd43b643bddea1966a43596e44e92b2ca475
Woshiajuana <zhigang.chen@owulia.com>
2019-09-06 10:25:53 +0800

    subcode

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -2,14 +2,8 @@
 
 'use strict';
 
-/**
- * @param {Egg.EggAppInfo} appInfo app info
- */
 module.exports = appInfo => {
-    /**
-     * built-in config
-     * @type {Egg.EggAppConfig}
-     **/
+
     const config = exports = {};
 
     // use for cookie sign key, should change to your own and keep security
@@ -27,6 +21,9 @@ module.exports = appInfo => {
             db: 0,
         },
     };
+
+    // add cors
+    
 
     // add email
     config.email = {
