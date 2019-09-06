e26ad9a9fda752e1708096b783839955c3a2eee4
Woshiajuana <zhigang.chen@owulia.com>
2019-09-06 10:25:43 +0800

    subcode

--- server/config/plugin.js
+++ server/config/plugin.js
@@ -2,26 +2,40 @@
 
 const path = require('path');
 
+// 页面模块
 exports.nunjucks = {
     enable: true,
     package: 'egg-view-nunjucks',
 };
 
+// redis
 exports.redis = {
     enable: true,
     package: 'egg-redis',
 };
 
+// 跨域
+exports.cors = {
+    enable: true,
+    package: 'egg-cors',
+};
+
+// 数据库mongoose
+export.
+
+// 发送邮件
 exports.email = {
     enable: true,
     path: path.join(__dirname, '../lib/plugin/egg-wow-email'),
 };
 
+// 验证参数
 exports.validate = {
     enable: true,
     path: path.join(__dirname, '../lib/plugin/egg-wow-validate'),
 };
 
+// 响应
 exports.response = {
     enable: true,
     path: path.join(__dirname, '../lib/plugin/egg-wow-response'),
