8ae98f51ea4640d4b2b5542af6bf1e9d71230387
Woshiajuana <zhigang.chen@owulia.com>
2019-09-09 19:38:26 +0800

    subcode

--- client/src/utils/http.util.js
+++ client/src/utils/http.util.js
@@ -12,22 +12,26 @@ class Http {
             baseURL: Config.baseURL,
             timeout: 10000,
         }, options);
-        this._curl();
+        return this._curl();
     }
 
     _curl () {
-        this._log('请求参数 => ', this.data);
-        axios({
-            url: this.api,
-            ...this.options,
-            data: this.data,
-            params: this.data,
-        }).then((res) => {
-            this._log('请求成功：返回参数 => ', res);
-            console.log(res);
-        }).catch((err) => {
-            this._log('请求失败：返回参数 => ', err);
-            console.log(err);
+        return new Promise((resolve, reject) => {
+            this._log('请求参数 => ', this.data);
+            axios({
+                url: this.api,
+                ...this.options,
+                data: this.data,
+                params: this.data,
+            }).then((res) => {
+                this._log('请求成功：返回参数 => ', res);
+                console.log(res);
+                resolve(res);
+            }).catch((err) => {
+                this._log('请求失败：返回参数 => ', err);
+                console.log(err);
+                reject(err);
+            });
         });
     }
 
