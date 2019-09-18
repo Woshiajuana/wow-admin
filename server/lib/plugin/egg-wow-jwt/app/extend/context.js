64b564f6b53f52539dc1165aa82f7a86e8acc8c1
Woshiajuana <zhigang.chen@owulia.com>
2019-09-18 10:39:12 +0800

    subcode

--- server/lib/plugin/egg-wow-jwt/app/extend/context.js
+++ server/lib/plugin/egg-wow-jwt/app/extend/context.js
@@ -1,22 +1,37 @@
 'use strict';
 
 const jwt = require('jsonwebtoken');
+const JWT = Symbol('Application#jwt');
 
 module.exports = {
-    jwt: {
 
-        async verify (jwtString, secretOrPublicKey, options) {
-            return new Promise((resolve, reject) => {
-                jwt.verify(jwtString, secretOrPublicKey, options, (res) => {
-                    resolve(res);
+    get jwt () {
+        if (!this[JWT]) {
+            const config = this.config.jwt;
+            console.log('config => ', config);
+            this[JWT] = {};
+            this[JWT].verify = async (jwtString, secretOrPublicKey, options) => {
+                return new Promise((resolve, reject) => {
+                    jwt.verify(
+                        jwtString,
+                        secretOrPublicKey,
+                        options,
+                        (res) => resolve(res)
+                    );
                 });
-            });
-        },
+            };
 
-        async sign () {
-
-
-
-        },
+            this[JWT].sign = async (payload, secretOrPrivateKey, options) => {
+                return new Promise((resolve, reject) => {
+                    jwt.sign(
+                        payload,
+                        secretOrPrivateKey,
+                        Object.assign({}, config, options),
+                        (res) => resolve(res)
+                    );
+                });
+            };
+        }
+        return this[JWT];
     }
 };
