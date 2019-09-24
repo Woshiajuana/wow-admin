f091dab7c61d343fd2c15f332abb9c9073d9659c
Woshiajuana <zhigang.chen@owulia.com>
2019-09-24 16:45:38 +0800

    subcode

--- client/src/views/admin/group/data.mixin.js
+++ client/src/views/admin/group/data.mixin.js
@@ -29,6 +29,10 @@ const data = () => {
                 text: '新增',
                 type: 'primary',
                 icon: 'el-icon-plus',
+                route: {
+                    path: '/',
+                    params: {},
+                },
                 event: 'add',
             }
         ],
