48ca778b85b3f8fcaffe3a71f7e86fa7f40ce355
Woshiajuana <zhigang.chen@owulia.com>
2019-10-14 16:36:14 +0800

    subcode

--- client/src/main.js
+++ client/src/main.js
@@ -199,7 +199,7 @@ let { wow, app } = window.wowRuntime.init({
     },
     // API配置
     httpRequest: {
-        baseURL: `${window.location.hostname.protocol}//${window.location.hostname}:7001/`,
+        baseURL: `${window.location.protocol}//${window.location.hostname}:7001/`,
         timeout: 3000,
     },
     // app 常量配置
