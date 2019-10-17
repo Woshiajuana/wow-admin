6135268977e681b9b0aa3b96409796d0a0d7a1c9
Woshiajuana <zhigang.chen@owulia.com>
2019-10-17 17:23:50 +0800

    subcode

--- server/lib/plugin/egg-wow-curl/lib/curl.js
+++ server/lib/plugin/egg-wow-curl/lib/curl.js
@@ -42,84 +42,24 @@ class CurlService extends Service {
         return data.data;
     }
 
-    async beforeRequest (opts) {
-
+    async beforeRequest (options) {
+        return options;
     }
 
-    async curl(url, opts) {
-        opts = Object.assign({}, this.opts.curlOptions, opts);
-        url = this.opts.apiUrl + url;
-        opts.method = opts.method || 'POST';
-        opts.dataType = opts.dataType || 'json';
-        const slience = opts.hasOwnProperty('slience') ? !!opts.slience : !!this.opts.slience;
-
-        const { logger } = this;
-
-        this.beforeRequest(opts);
-
-        let logData = {};
-        if (!slience) {
-            logData = Object.assign(logData, opts.data);
-
-            if (opts.logIgnore) {
-                if (typeof opts.logIgnore === 'string') {
-                    logData[opts.logIgnore] = '***';
-                } else if (Array.isArray(opts.logIgnore)) {
-                    opts.logIgnore.forEach(key => {
-                        if (logData.hasOwnProperty(key)) {
-                            logData[key] = '***';
-                        }
-                    });
-                }
-            }
-        }
-
-        if (!slience) {
-            logger.info(`调用 ${this.opts.apiName} server url: ${url} method: ${opts.method} data: ${JSON.stringify(logData)}`);
-        }
-
-        let response = null;
-        let responseErr = null;
-
-        try {
-            response = await this.ctx.curl(url, opts);
-        } catch (err) {
-            logger.info(`调用 ${this.opts.apiName} server url: ${url} 返回异常出错 Error: ${err.message} ${err.stack}`);
-            throw err;
-        }
-
-        if (!response || !response.data) {
-            logger.info(`调用 ${this.opts.apiName} 返回为空`);
-            responseErr = new Error('返回格式错误');
-            throw responseErr;
-        }
-
-        let data = null;
-
-        if (typeof this.afterRequest === 'function') {
-            try {
-                data = this.afterRequest(response, opts);
-            } catch (err) {
-                logger.info(`调用 ${this.opts.apiName} server url: ${url} 失败返回数据  status:${response.status} ${JSON.stringify(response.data)} Error: ${err.message}`);
-                throw err;
-            }
-
-            if (!slience) {
-                logger.info(`调用 ${this.opts.apiName} server url: ${url} 成功返回数据 status: ${response.status}`);
-            }
-
-            return data;
-        }
-
-        if (!slience) {
-            logger.info(`调用 ${this.opts.apiName} server url: ${url} 成功返回数据 status: ${response.status}`);
-        }
-
-        return response.data;
+    async curl (url, options) {
+        let {
+            baseUrl,
+        } = options = Object.assign({
+            method: 'POST',
+            dataType: 'json',
+        }, this.options, options);
+        url = `${baseUrl}${url}`;
+        options = await this.beforeRequest(options);
+        let response = await this.ctx.curl(url, options);
+        response = await this.afterRequest(response);
+        return response;
     }
 }
-
-console.log('执行了执行了执行了')
 
 Object.assign(egg, {
     CurlService,
