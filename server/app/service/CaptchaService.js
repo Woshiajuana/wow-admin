e9b23c011ab90caeaa9d03f4b46ff4d946544578
Woshiajuana <zhigang.chen@owulia.com>
2019-11-12 11:18:31 +0800

    subcode

--- server/app/service/CaptchaService.js
+++ server/app/service/CaptchaService.js
@@ -9,7 +9,9 @@ const { Service } = require('egg');
 module.exports = class HandleServer extends Service {
 
     // 生成图形验证码
-    async generate (email) {
+    async generate (key) {
+        const { app, logger } = this;
+        const { redis } = app;
         let text = randomNum(6);
         let captchaPng = new CaptchaPng(80, 30, text);
         captchaPng.color(255, 255, 255, 0);  // First color: background (red, green, blue, alpha)
@@ -25,9 +27,23 @@ module.exports = class HandleServer extends Service {
         //     noise: 2, // 干扰线条的数量
         //     height: 44
         // });
-        await redisUtil.setItem(`${email} captcha`, text);
-        return data;
+        logger.info(`【${key}】生成图形验证码:【${text}】`);
+        await redis.set(`${key} captcha`, JSON.stringify({ value: text, times: 0 }));
     }
+
+    // 验证验证图形码
+    async firewall (key, value) {
+        const { app, logger } = this;
+        const { redis } = app;
+        let captcha = await redis.get(`${key} captcha`);
+        if (!captcha) return null;
+        captcha = JSON.parse(captcha);
+        await this.filterTimes(email);
+        if (text !== captcha)
+            throw '图形验证码错误';
+        
+    },
+
 };
 
 function randomNum (len) {
