67347a118b31a4f91bae4285ca7a4fdcd7cbfa17
Woshiajuana <zhigang.chen@owulia.com>
2019-11-13 10:08:03 +0800

    subcode

--- client/src/views/login/index.mixin.js
+++ client/src/views/login/index.mixin.js
@@ -5,7 +5,7 @@ const data = () => {
 
         objForm: {
             account: {
-                value: 'admin',
+                value: '',
                 icon: 'user',
                 max: 50,
                 placeholder: '请输入账号',
@@ -17,7 +17,7 @@ const data = () => {
                 ],
             },
             password: {
-                value: '1',
+                value: '',
                 type: 'password',
                 icon: 'password',
                 max: 20,
