8c3f4f7eaf7f5d93d5da1c0ad47e04a4e3d73526
Woshiajuana <zhigang.chen@owulia.com>
2020-01-07 16:36:39 +0800

    subcode code

--- client/src/main.js
+++ client/src/main.js
@@ -198,28 +198,28 @@ window.wowRuntime = {
 };
 
 
-let { wow, app } = window.wowRuntime.init({
-    // 扩展类配置, 这个类里面的数据都会扩展挂载到 VUE 上
-    extendUtils: {
-
-    },
-    // API配置
-    httpRequest: {
-        baseURL: `${window.location.protocol}//${window.location.hostname}:7001/`,
-        timeout: 3 * 1000,
-    },
-    // app 常量配置
-    appConst: {
-
-    },
-    // 路由配置
-    routerConfig: {
-
-    },
-    // 组件配置
-    component: {
-        importComponents: [ ],
-    },
-});
-
-console.log(wow);
+// let { wow, app } = window.wowRuntime.init({
+//     // 扩展类配置, 这个类里面的数据都会扩展挂载到 VUE 上
+//     extendUtils: {
+//
+//     },
+//     // API配置
+//     httpRequest: {
+//         baseURL: `${window.location.protocol}//${window.location.hostname}:7001/`,
+//         timeout: 3 * 1000,
+//     },
+//     // app 常量配置
+//     appConst: {
+//
+//     },
+//     // 路由配置
+//     routerConfig: {
+//
+//     },
+//     // 组件配置
+//     component: {
+//         importComponents: [ ],
+//     },
+// });
+//
+// console.log(wow);
