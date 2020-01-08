c3c4a68ba6abe944ed8965953a6d1238626363cf
Woshiajuana <zhigang.chen@owulia.com>
2020-01-08 13:44:10 +0800

    subcode code

--- client/src/views/menu/menu1/menu1-2/index.vue
+++ client/src/views/menu/menu1/menu1-2/index.vue
@@ -2,7 +2,6 @@
   <div style="padding:30px;">
     <el-alert :closable="false" title="menu 1-2" type="success">
       <router-view />
-
         <span @click="handleJump">跳转到1-3</span>
     </el-alert>
   </div>
